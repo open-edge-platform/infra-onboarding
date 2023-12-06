114292569edb49bab092d6bf74825e19ea1a8907
shafiudx <146054677+shafiudx@users.noreply.github.com>
2023-12-06 19:54:09 +0530

    Added profile sample request file to trigger onboarding (#20)
    Co-authored-by: Rishi Raj Gupta <rishi.raj1.gupta@intel.com>

--- mi-onboarding-manager/internal/provisioningservice/test/client/client_eb.go
+++ mi-onboarding-manager/internal/provisioningservice/test/client/client_eb.go
@@ -2,20 +2,63 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 
 	pbi "github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.services.managers.onboarding/api/grpc/onboardingmgr"
 	pb "github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.services.managers.onboarding/api/grpc/provisioningproto"
 	"google.golang.org/grpc"
+	"gopkg.in/yaml.v2"
 )
 
-const (
-	address = "localhost:50054"
-)
+// Struct to hold input configuration from YAML
+type InputConfig struct {
+	ArtifactData []struct {
+		Name        string `yaml:"name"`
+		Version     string `yaml:"version"`
+		Platform    string `yaml:"platform"`
+		Category    int    `yaml:"category"`
+		Description string `yaml:"description"`
+		Details     struct {
+			Name    string `yaml:"name"`
+			Url     string `yaml:"url"`
+			Contact string `yaml:"contact"`
+		} `yaml:"details"`
+		PackageUrl   string `yaml:"packageUrl"`
+		Author       string `yaml:"author"`
+		License      string `yaml:"license"`
+		Vendor       string `yaml:"vendor"`
+		Manufacturer string `yaml:"manufacturer"`
+		ReleaseData  string `yaml:"releaseData"`
+		ArtifactId   string `yaml:"artifactId"`
+		Result       int    `yaml:"result"`
+	} `yaml:"artifactData"`
+
+	HwData []struct {
+		HwId      string `yaml:"hwid"`
+		MacId     string `yaml:"macid"`
+		SutIp     string `yaml:"sutip"`
+		CusParams struct {
+			DpsScopeId          string `yaml:"dpsscopeid"`
+			DpsRegistrationId   string `yaml:"dpsregistrationid"`
+			DpsEnrollmentSymKey string `yaml:"dpsenrollmentsymkey"`
+		} `yaml:"cusparams"`
+		DiskPartition string `yaml:"disk"`
+		PlatformType  string `yaml:"platformtype"`
+	} `yaml:"hwdata"`
+
+	OnbParams struct {
+		PdIp           string `yaml:"pdip"`
+		PdMac          string `yaml:"pdmac"`
+		LoadBalancerIp string `yaml:"loadbalancerip"`
+		DiskPartition  string `yaml:"diskpartition"`
+		Env            string `yaml:"env"`
+	} `yaml:"onboarding"`
+}
 
 func generateDevSerial(macID string) (string, error) {
 	// Remove colons from the MAC address
@@ -46,41 +89,70 @@ func generateRandomString(length int) string {
 func OnboardingTest(client pb.OnBoardingEBClient) (*pb.OnboardingResponse, error) {
 	var obm pb.OnboardingRequest
 	log.Printf("start onboarding")
-
-	devSerial, dev_ser_err := generateDevSerial("1c:69:7a:0e:b2:28")
-	if dev_ser_err != nil {
-		fmt.Printf("Error: %v\n", dev_ser_err)
-		return nil, dev_ser_err
+	dirPath, _ := os.Getwd()
+	// Read YAML file
+	yamlData, err := ioutil.ReadFile(dirPath + "/profile_sample.yaml")
+	if err != nil {
+		log.Fatalf("Error reading YAML file: %v", err)
+		return nil, err
 	}
-	log.Printf("Device Serial number %s", devSerial)
 
-	obm.Hwdata = append(obm.Hwdata, &pbi.HwData{
-		HwId:  devSerial,
-		MacId: "",
-		SutIp: "",
-		CusParams: &pbi.CustomerParams{
-			DpsScopeId:          "Add dps scopeid",
-			DpsRegistrationId:   "registration id",
-			DpsEnrollmentSymKey: "Add DpsEnrollmentSymKey from your azure portal ",
-		},
-		DiskPartition: "/dev/sda",
-		PlatformType:  "prod_focal-ms",
-	})
+	// Unmarshal YAML data into struct
+	var inputConfig InputConfig
+	err = yaml.Unmarshal(yamlData, &inputConfig)
+	if err != nil {
+		log.Fatalf("Error unmarshalling YAML: %v", err)
+		return nil, err
+	}
 
-	obm.Hwdata = append(obm.Hwdata, &pbi.HwData{
-		HwId:  devSerial,
-		MacId: "",
-		SutIp: "",
-		CusParams: &pbi.CustomerParams{
-			DpsScopeId:          "Add dps scopeid",
-			DpsRegistrationId:   "registration id",
-			DpsEnrollmentSymKey: "Add DpsEnrollmentSymKey from your azure portal ",
-		},
-		DiskPartition: "/dev/sda",
-		PlatformType:  "prod_focal-ms",
-	})
+	// Iterate through artifact data and populate obm
+	for _, artifactData := range inputConfig.ArtifactData {
+		obm.ArtifactData = append(obm.ArtifactData, &pbi.ArtifactData{
+			Name:        artifactData.Name,
+			Version:     artifactData.Version,
+			Platform:    artifactData.Platform,
+			Category:    pbi.ArtifactData_ArtifactCategory(artifactData.Category),
+			Description: artifactData.Description,
+			Details: &pbi.Supplier{
+				Name:    artifactData.Details.Name,
+				Url:     artifactData.Details.Url,
+				Contact: artifactData.Details.Contact,
+			},
+			PackageUrl:   artifactData.PackageUrl,
+			Author:       artifactData.Author,
+			License:      artifactData.License,
+			Vendor:       artifactData.Vendor,
+			Manufacturer: artifactData.Manufacturer,
+			ReleaseData:  artifactData.ReleaseData,
+			ArtifactId:   artifactData.ArtifactId,
+			Result:       pbi.ArtifactData_Response(artifactData.Result),
+		})
+	}
 
-	obm.OnbParams = &pbi.OnboardingParams{PdIp: "", PdMac: "", LoadBalancerIp: "", DiskPartition: "/dev/sda", Env: "ZT"}
+	// Iterate through hardware data and populate obm
+	for _, hwData := range inputConfig.HwData {
+		obm.Hwdata = append(obm.Hwdata, &pbi.HwData{
+			HwId:  hwData.HwId,
+			MacId: hwData.MacId,
+			SutIp: hwData.SutIp,
+			CusParams: &pbi.CustomerParams{
+				DpsScopeId:          hwData.CusParams.DpsScopeId,
+				DpsRegistrationId:   hwData.CusParams.DpsRegistrationId,
+				DpsEnrollmentSymKey: hwData.CusParams.DpsEnrollmentSymKey,
+			},
+			DiskPartition: hwData.DiskPartition,
+			PlatformType:  hwData.PlatformType,
+		})
+	}
+
+	// Populate onboarding parameters
+	obm.OnbParams = &pbi.OnboardingParams{
+		PdIp:           inputConfig.OnbParams.PdIp,
+		PdMac:          inputConfig.OnbParams.PdMac,
+		LoadBalancerIp: inputConfig.OnbParams.LoadBalancerIp,
+		DiskPartition:  inputConfig.OnbParams.DiskPartition,
+		Env:            inputConfig.OnbParams.Env,
+	}
 	// Add other variables in onboarding request
 	res, err := client.StartOnboarding(context.Background(), &obm)
 	if err != nil {
@@ -91,6 +163,15 @@ func OnboardingTest(client pb.OnBoardingEBClient) (*pb.OnboardingResponse, error
 }
 
 func main() {
+	onb_addr := os.Getenv("MGR_HOST")
+	onb_port := os.Getenv("ONBMGR_PORT")
+	address := onb_addr + ":" + onb_port
+
+	if onb_addr == "" || onb_port == "" {
+		log.Printf("Invalid environment variables MGR_HOST and ONBMGR_PORT please export")
+		os.Exit(1)
+	}
+
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Did not connect: %v", err)
