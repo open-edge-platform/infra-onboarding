85380d08a1a40bf8571aa15b9ea94ef35d07abd2
punam20 <102661080+punam20@users.noreply.github.com>
2024-03-20 01:24:07 +0530

    DKAM: watcher for k8s secret (#103)
    Add watcher to notify on any change in  boots and TLS certificates.
    On any change in these certificates restart DKAM.
    
    Signed-off-by: punam biswal <punam.biswal@intel.com>
    Co-authored-by: Daniele Moro <daniele.moro@intel.com>

--- mi-dkam/cmd/dkammgr/main.go
+++ mi-dkam/cmd/dkammgr/main.go
@@ -7,7 +7,9 @@ import (
 
 	"flag"
 	"net"
+	"os"
 
+	"github.com/fsnotify/fsnotify"
 	pb "github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.dkam-service/api/grpc/dkammgr"
 	"github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.dkam-service/internal/dkammgr"
 	"github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.dkam-service/pkg/config"
@@ -21,6 +23,15 @@ var (
 )
 
 func main() {
+
+	// Create a certificate watcher
+	watcher, watcherErr := SetWatcher()
+	if watcherErr != nil {
+		zlog.MiSec().Fatal().Err(watcherErr).Msgf("Failed to set watcher.")
+		return
+	}
+	defer watcher.Close()
+
 	//Download OS image
 	downloadErr := dkammgr.DownloadOS()
 	if downloadErr != nil {
@@ -29,11 +40,10 @@ func main() {
 	}
 
 	//Download release manifest.yaml file.
-	err := dkammgr.DownloadArtifacts()
-	if err != nil {
-		zlog.MiSec().Info().Msgf("Failed to download manifest")
-		//zlog.MiSec().Fatal().Err(err).Msgf("Error downloading file")
-		//return
+	artifactsErr := dkammgr.DownloadArtifacts()
+	if artifactsErr != nil {
+		zlog.MiSec().Fatal().Err(artifactsErr).Msgf("Error downloading file")
+		return
 	}
 
 	//Donwload and sign iPXE
@@ -71,5 +81,57 @@ func main() {
 	zlog.Info().Msgf("Starting gRPC server on port %s", *servaddr)
 	if err := grpc_server.Serve(lis); err != nil {
 		zlog.MiSec().Fatal().Err(err).Msgf("Failed to serve: %v", err)
+	}
+
+	select {}
+}
+
+func SetWatcher() (*fsnotify.Watcher, error) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		zlog.MiSec().Fatal().Err(err).Msgf("Failed to create a watcher")
+	}
+
+	orchCACertificateFile := config.OrchCACertificateFile
+	bootsCaCertificateFile := config.BootsCaCertificateFile
+
+	// Add the certificate files to the watcher
+	addFileToWatcher(watcher, orchCACertificateFile)
+	addFileToWatcher(watcher, bootsCaCertificateFile)
+
+	// Watch for events
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					zlog.MiSec().Info().Msg("Certificate file changed. Rebuilding iPXE and microOS...")
+					zlog.MiSec().Fatal().Err(err).Msgf("Restart DKAM: %v", err)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				zlog.MiSec().Fatal().Err(err).Msgf("Error:%v", err)
+			}
+		}
+	}()
+
+	return watcher, nil
+
+}
+
+func addFileToWatcher(watcher *fsnotify.Watcher, filename string) {
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		zlog.MiSec().Fatal().Err(err).Msgf("File does not exist:%s, error: %v", filename, err)
+		return
+	}
+	if err := watcher.Add(filename); err != nil {
+		zlog.MiSec().Error().Msgf("Failed to add file to watcher:%s, error: %v", filename, err)
+	} else {
+		zlog.MiSec().Info().Msgf("Watcher added for file:%s", filename)
 	}
 }
