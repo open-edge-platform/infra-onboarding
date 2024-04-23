a05aff5220192db83dd1c4ee77bf634d956d662a
shankarsrinivas1 <shankar.srinivas.jayanthi@intel.com>
2024-04-23 16:54:22 +0530

    This is to boot to hardidk after successul provision (#109)

--- tinker-actions/tinker_actions/efibootset/efibootset.sh
+++ tinker-actions/tinker_actions/efibootset/efibootset.sh
@@ -48,16 +48,33 @@ pxe_boot_number=$(efibootmgr | grep -i "Bootcurrent" | awk '{print $2}')
 
 boot_order=$(efibootmgr | grep -i "Bootorder" | awk '{print $2}')
 
-remove_pxe=$(sed "s/$pxe_boot_number//g" <<< $boot_order)
-remove_pxe=$(sed "s/,,/,/g" <<< $remove_pxe)
+# Convert boot_order to an array and remove , between the entries
+IFS=',' read -ra boot_order_array <<< "$boot_order"
 
-final_bootorder=$remove_pxe","$pxe_boot_number
-final_bootorder=$(sed "s/,,/,/g" <<< $final_bootorder)
-#check if comma or some invalid boot number at the begining
-if [ "${final_bootorder:0:1}" = "," ]; then
-    # Remove the comma
-    final_bootorder="${final_bootorder:1}"
-fi
-efibootmgr --bootorder $final_bootorder
-echo "Made PXE the last in the boot order"
+# Remove PXE boot entry from Array
+final_boot_array=()
+for element in "${boot_order_array[@]}"; do
+    if [[ "$element" != "$pxe_boot_number" ]]; then
+        final_boot_array+=("$element")
+    fi
+done
+
+# Add the PXE  boot entry to the end of the boot order array
+final_boot_array+=("$pxe_boot_number")
+
+# Join the elements of boot_order_array into a comma-separated string
+final_boot_order=$(IFS=,; echo "${final_boot_array[*]}")
+
+#remove trail and leading , if preset
+final_boot_order=$(echo "$final_boot_order" | sed -e  's/^,//;s/,$//' )
+
+echo "final_boot order--->" $final_boot_order
+
+# Update the boot order using efibootmgr
+efibootmgr -o "$final_boot_order"
+
+#Make UEFI boot as inactive 
+efibootmgr -b $pxe_boot_number -A
+
+echo "Made Disk as first boot and PXE boot order at end"
 # #####################################################################################
