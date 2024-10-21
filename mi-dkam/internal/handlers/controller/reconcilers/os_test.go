02a09dbf8bf1c7031f0040ff275c54e6d34c3e61
yvuppalx <146812376+yvuppalx@users.noreply.github.com>
2024-10-21 18:46:20 +0530

    unit test cases for os (#311)
    * unit test cases
    
    * removed unwanted code

--- mi-dkam/internal/handlers/controller/reconcilers/os_test.go
+++ mi-dkam/internal/handlers/controller/reconcilers/os_test.go
@@ -5,14 +5,16 @@ package reconcilers
 
 import (
 	"context"
-	"github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.services.inventory/v2/pkg/client"
+	"io"
 	"os"
 	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
 
 	rec_v2 "github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-app.lib-go/pkg/controller/v2"
 	"github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.dkam-service/internal/invclient"
+	"github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.dkam-service/pkg/config"
 	dkam_testing "github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.dkam-service/testing"
 	osv1 "github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.services.inventory/v2/pkg/api/os/v1"
 	inv_testing "github.com/intel-innersource/frameworks.edge.one-intel-edge.maestro-infra.services.inventory/v2/pkg/testing"
@@ -64,7 +66,30 @@ func TestNewOsReconciler(t *testing.T) {
 	}
 }
 
-func TestOsReconciler_Reconcile(t *testing.T) {
+func CopyFile(src, dst string) error {
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+	if err := os.MkdirAll(filepath.Dir(src), 0755); err != nil {
+		return err
+	}
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestOsReconcilerReconcile(t *testing.T) {
 	type fields struct {
 		invClient     *invclient.DKAMInventoryClient
 		enableTracing bool
@@ -77,29 +102,46 @@ func TestOsReconciler_Reconcile(t *testing.T) {
 	t.Cleanup(func() {
 		dkam_testing.DeleteInventoryDKAMClientForTesting()
 	})
+	osre := inv_testing.CreateOsWithArgs(t, "", "profile:profile", osv1.SecurityFeature_SECURITY_FEATURE_NONE, osv1.OsType_OS_TYPE_MUTABLE)
 	testRequest := rec_v2.Request[ReconcilerID]{
-		ID: WrapReconcilerID(client.FakeTenantID, "test-id"),
+		ID: ReconcilerID(WrapReconcilerID(osre.TenantId, osre.ResourceId)),
 	}
-
+	testRequest1 := rec_v2.Request[ReconcilerID]{
+		ID: ReconcilerID(WrapReconcilerID(osre.TenantId, "os-12345")),
+	}
+	rawFileName := strings.TrimSuffix(osre.ProfileName, ".img") + ".raw.gz"
+	expectedFilePath := config.PVC + "/OSImage/" + osre.Sha256 + "/" + rawFileName
+	err := os.MkdirAll(filepath.Dir(expectedFilePath), 0755)
+	if err != nil {
+		t.Fatalf("Failed to create directories: %v", err)
+	}
+	file, err := os.Create(expectedFilePath)
+	if err != nil {
+		t.Fatalf("Failed to create file: %v", err)
+	}
+	file.Close()
+	//
+	filePath := config.PVC + "/OSArtifacts/" + osre.ResourceId + "/installer.sh"
+	dir := filepath.Dir(filePath)
+	mkerr := os.MkdirAll(dir, 0755)
+	if mkerr != nil {
+		t.Fatalf("Failed to create directories: %v", mkerr)
+	}
+	file, cerr := os.Create(filePath)
+	if cerr != nil {
+		t.Fatalf("Failed to create file: %v", cerr)
+	}
+	defer file.Close()
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		t.Fatalf("File was not created")
+	}
 	tests := []struct {
 		name   string
 		fields fields
 		args   args
-		want   rec_v2.Directive[ReconcilerID]
 	}{
 		{
-			name: "TestOsReconciler_ReconcileWithErrorFetchingResource",
-			fields: fields{
-				invClient: dkam_testing.InvClient,
-			},
-			args: args{
-				ctx:     context.TODO(),
-				request: testRequest,
-			},
-			want: testRequest.Ack(),
-		},
-		{
-			name: "Test Os reconciler -reconcileWith successful resource Fetch",
+			name: "TestOsReconciler with Mutable os",
 			fields: fields{
 				invClient:     dkam_testing.InvClient,
 				enableTracing: true,
@@ -108,7 +150,17 @@ func TestOsReconciler_Reconcile(t *testing.T) {
 				ctx:     context.TODO(),
 				request: testRequest,
 			},
-			want: testRequest.Ack(),
+		},
+		{
+			name: "Test Case",
+			fields: fields{
+				invClient:     dkam_testing.InvClient,
+				enableTracing: false,
+			},
+			args: args{
+				ctx:     context.Background(),
+				request: testRequest1,
+			},
 		},
 	}
 	for _, tt := range tests {
@@ -117,11 +169,21 @@ func TestOsReconciler_Reconcile(t *testing.T) {
 				invClient:     tt.fields.invClient,
 				enableTracing: tt.fields.enableTracing,
 			}
-			if got := osr.Reconcile(tt.args.ctx, tt.args.request); reflect.DeepEqual(got, tt.want) {
-				t.Errorf("OsReconciler.Reconcile() = %v, want %v", got, tt.want)
-			}
+			osr.Reconcile(tt.args.ctx, tt.args.request)
 		})
 	}
+	defer func() {
+		err := os.Remove(expectedFilePath)
+		if err != nil && !os.IsNotExist(err) {
+			t.Fatalf("Failed to remove file: %v", err)
+		}
+		err = os.RemoveAll(filepath.Dir(expectedFilePath))
+		if err != nil {
+			t.Fatalf("Failed to clean up directories: %v", err)
+		}
+		os.Remove(expectedFilePath)
+		os.Remove(dir + "/installer.sh")
+	}()
 }
 
 func TestIsSameOSResource(t *testing.T) {
@@ -167,6 +229,130 @@ func TestIsSameOSResource(t *testing.T) {
 			if got != tt.want {
 				t.Errorf("IsSameOSResource() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestOsReconciler_Reconcile(t *testing.T) {
+	type fields struct {
+		invClient     *invclient.DKAMInventoryClient
+		enableTracing bool
+	}
+	type args struct {
+		ctx     context.Context
+		request rec_v2.Request[ReconcilerID]
+	}
+	dkam_testing.CreateInventoryDKAMClientForTesting()
+	t.Cleanup(func() {
+		dkam_testing.DeleteInventoryDKAMClientForTesting()
+	})
+	osre := inv_testing.CreateOsWithArgs(t, "", "profile:profile", osv1.SecurityFeature_SECURITY_FEATURE_NONE, osv1.OsType_OS_TYPE_IMMUTABLE)
+	testRequest := rec_v2.Request[ReconcilerID]{
+		ID: ReconcilerID(WrapReconcilerID(osre.TenantId, osre.ResourceId)),
+	}
+	expectedFilePath := config.DownloadPath + "/" + config.TiberOSImage
+	err := os.MkdirAll(filepath.Dir(expectedFilePath), 0755)
+	if err != nil {
+		t.Fatalf("Failed to create directories: %v", err)
+	}
+	file, err := os.Create(expectedFilePath)
+	if err != nil {
+		t.Fatalf("Failed to create file: %v", err)
+	}
+	file.Close()
+	///
+	originalDir, _ := os.Getwd()
+	result := strings.Replace(originalDir, "reconcilers", "script/tmp", -1)
+	res := filepath.Join(result, "latest-dev.yaml")
+	if err := os.MkdirAll(filepath.Dir(res), 0755); err != nil {
+		t.Fatalf("Failed to create directory: %v", err)
+	}
+	src := strings.Replace(originalDir, "internal/handlers/controller/reconcilers", "pkg/script/latest-dev.yaml", -1)
+	CopyFile(src, res)
+	direc := config.DownloadPath + "/tmp/"
+	os.MkdirAll(direc, 0755)
+	os.Create(direc + "latest-dev.yaml")
+	CopyFile(src, direc+"latest-dev.yaml")
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   rec_v2.Directive[ReconcilerID]
+	}{
+		{
+			name: "test case with immutable os",
+			fields: fields{
+				invClient:     dkam_testing.InvClient,
+				enableTracing: true,
+			},
+			args: args{
+				ctx:     context.Background(),
+				request: testRequest,
+			},
+			want: testRequest.Ack(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			osr := &OsReconciler{
+				invClient:     tt.fields.invClient,
+				enableTracing: tt.fields.enableTracing,
+			}
+			if got := osr.Reconcile(tt.args.ctx, tt.args.request); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("OsReconciler.Reconcile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+	tmpDir := strings.Replace(originalDir, "reconcilers", "script", -1)
+	defer func() {
+		CopyFile(res, src)
+		os.RemoveAll(config.PVC)
+		os.RemoveAll(originalDir + "/tmp")
+		os.RemoveAll(tmpDir)
+	}()
+}
+
+func TestOsReconcilerReconcile_DownloadOs_Err(t *testing.T) {
+	type fields struct {
+		invClient     *invclient.DKAMInventoryClient
+		enableTracing bool
+	}
+	type args struct {
+		ctx     context.Context
+		request rec_v2.Request[ReconcilerID]
+	}
+	dkam_testing.CreateInventoryDKAMClientForTesting()
+	t.Cleanup(func() {
+		dkam_testing.DeleteInventoryDKAMClientForTesting()
+	})
+	osre := inv_testing.CreateOsWithArgs(t, "", "profile:profile", osv1.SecurityFeature_SECURITY_FEATURE_NONE, osv1.OsType_OS_TYPE_MUTABLE)
+	testRequest := rec_v2.Request[ReconcilerID]{
+		ID: ReconcilerID(WrapReconcilerID(osre.TenantId, osre.ResourceId)),
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "TestOsReconciler with Mutable os",
+			fields: fields{
+				invClient:     dkam_testing.InvClient,
+				enableTracing: true,
+			},
+			args: args{
+				ctx:     context.TODO(),
+				request: testRequest,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			osr := &OsReconciler{
+				invClient:     tt.fields.invClient,
+				enableTracing: tt.fields.enableTracing,
+			}
+			osr.Reconcile(tt.args.ctx, tt.args.request)
 		})
 	}
 }
