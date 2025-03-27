8d6203a117a9e917f0c5586b1f8090181675b456
ppanigra <106302035+ppanigra@users.noreply.github.com>
2025-03-27 20:13:00 +0530

    Revert "Rename fde tinker action to enable_security" (#215)
    Revert "Rename fde tinker action to enable_security (#213)"
    
    This reverts commit 1e552e12e0a200de5054334f9f1e42362031dbd5.

--- tinker-actions/src/fde/os_selection.sh
+++ tinker-actions/src/fde/os_selection.sh
@@ -17,7 +17,7 @@ get_partition_suffix() {
 
 #####################################################################################
 #top level script to check and run fde for ubuntu or Edge Microvisor Toolkit
-run_enable_security()
+run_enable_fde()
 {
     disk_device=""
 
@@ -58,8 +58,8 @@ run_enable_security()
     if [ $ubuntu_found -eq 0 ];
     then
 	# fde for ubuntu
-	echo "Ubuntu detected. running enable_fde_ubuntu."
-	bash enable_fde_ubuntu.sh
+	echo "Ubuntu detected. running enable_fde."
+	bash enable_fde.sh
     else
 
 	if [ -z "${ENABLE_ONLY_DMVERITY+x}" ] || [ "$ENABLE_ONLY_DMVERITY" = "false" ];
@@ -79,4 +79,4 @@ run_enable_security()
 }
 
 #####################################################################################
-run_enable_security
+run_enable_fde
