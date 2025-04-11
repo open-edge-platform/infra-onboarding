5e92483fb7193c34fe48f34e28cd74c5eed5fc9e
Zack Williams <zdw@users.noreply.github.com>
2025-04-11 02:43:06 -0700

    feat: Update Makefile and prepare for buildall (#17)
    * Update Makefile and prepare for buildall
    
    * bump version
    
    * Remove duplicate 'all' target
    
    * Skip trivy for 'postgres:16.4' testing-only image

--- version.mk
+++ version.mk
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
- 
+
 GOLINTVERSION_HAVE             := $(shell golangci-lint version | sed 's/.*version //' | sed 's/ .*//')
 GOLINTVERSION_REQ              := 1.64.5
 GOJUNITREPORTVERSION_HAVE      := $(shell go-junit-report -version | sed s/.*" v"// | sed 's/ .*//')
