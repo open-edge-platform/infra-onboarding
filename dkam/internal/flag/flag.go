b81b35694ce2687a7d1175fabe1bc1270fccc4ed
Tomasz Osinski <tomasz.osinski@intel.com>
2025-06-04 14:16:28 +0200

    refactor: Remove legacy Installer script from DKAM

--- dkam/internal/flag/flag.go
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: (C) 2025 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-
-package flag
-
-import "flag"
-
-var LegacyMode = flag.Bool("legacyMode", false,
-	"Set to enable curation of legacy Installer script for mutable OSes that will be pushed to PV.")
--- dkam/testing/testing_utils.go
+++ dkam/testing/testing_utils.go
@@ -14,8 +14,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	"github.com/open-edge-platform/infra-onboarding/dkam/internal/env"
-	"github.com/open-edge-platform/infra-onboarding/dkam/internal/flag"
 	"github.com/open-edge-platform/infra-onboarding/dkam/pkg/config"
 )
 
@@ -39,14 +37,6 @@ func exampleManifest(digest string, fileLen int) string {
 		"annotations":{"org.opencontainers.image.created":"2025-03-18T16:44:00Z"}}`, fileLen)
 }
 
-func EnableLegacyModeForTesting(t *testing.T) {
-	t.Helper()
-	*flag.LegacyMode = true
-	t.Cleanup(func() {
-		*flag.LegacyMode = false // restore default mode
-	})
-}
-
 func StartTestReleaseService(testProfileName string) func() {
 	config.SetInfraConfig(config.InfraConfig{
 		ENManifestRepo:     TestManifestRepo,
@@ -54,7 +44,6 @@ func StartTestReleaseService(testProfileName string) func() {
 	})
 	infraConfig := config.GetInfraConfig()
 
-	expectedFileContent := "GOOD TEST!"
 	expectedTestManifest := `
 repository:
   codename: 3.0
@@ -121,19 +110,6 @@ packages:
 		func(w http.ResponseWriter, _ *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			// empty data
-		})
-
-	// test handlers for profile script
-	mux.HandleFunc("/v2/"+env.ProfileScriptRepo+testProfileName+"/manifests/1.0.2",
-		func(w http.ResponseWriter, _ *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			// return example manifest
-			w.Write([]byte(exampleManifest(testProfileManifest, len(expectedFileContent))))
-		})
-	mux.HandleFunc("/v2/"+env.ProfileScriptRepo+testProfileName+"/blobs/"+testProfileManifest,
-		func(w http.ResponseWriter, _ *http.Request) {
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(expectedFileContent))
 		})
 
 	svr := httptest.NewServer(mux)
