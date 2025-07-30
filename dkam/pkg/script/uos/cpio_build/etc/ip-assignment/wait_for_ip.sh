6dd2358d6c14d433f08c8b4adcab712790a8de99
Panigrahi, Piyush <piyush.panigrahi@intel.com>
2025-07-30 18:01:49 +0800

    refactor for bash

--- dkam/pkg/script/uos/cpio_build/etc/ip-assignment/wait_for_ip.sh
+++ dkam/pkg/script/uos/cpio_build/etc/ip-assignment/wait_for_ip.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
+# SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
 
-#read /proc/cmdline and extract string after worker_id
-for i in $(cat $(pwd)/cmdline); do
-    if [ "${i#worker_id=}" != "$i" ]; then
-        worker_id=${i#worker_id=}
+# Read /proc/cmdline and extract string after worker_id
+worker_id=""
+for i in $(cat "/proc/cmdline"); do
+    if [[ "$i" == worker_id=* ]]; then
+        worker_id="${i#worker_id=}"
         break
     fi
 done
@@ -13,18 +16,18 @@ echo "Worker ID: ${worker_id}"
 SLEEP_TIME=3
 NUMBER_OF_RETRIES=10
 
-#check if IP address is assigned to interface matching MAC address with worker_id
+# Check if IP address is assigned to interface matching MAC address with worker_id
 for iface in $(ls /sys/class/net); do
-    mac_address=$(cat /sys/class/net/$iface/address)
-    for attempt in $(seq 1 $NUMBER_OF_RETRIES); do
-        if [ "$mac_address" = "$worker_id" ]; then
-            ip_address=$(ip addr show $iface | grep -w inet | awk '{print $2}' | cut -d/ -f1)
-            if [ -n "$ip_address" ]; then
+    mac_address=$(cat "/sys/class/net/$iface/address")
+    for ((attempt=1; attempt<=NUMBER_OF_RETRIES; attempt++)); do
+        if [[ "$mac_address" == "$worker_id" ]]; then
+            ip_address=$(ip addr show "$iface" | awk '/inet / {print $2}' | cut -d/ -f1)
+            if [[ -n "$ip_address" ]]; then
                 echo "IP Address $ip_address is assigned to interface $iface with MAC $mac_address"
                 exit 0
             else
                 echo "Attempt $attempt/$NUMBER_OF_RETRIES: No IP address assigned to interface $iface with MAC $mac_address yet"
-                sleep $SLEEP_TIME
+                sleep "$SLEEP_TIME"
             fi
         fi
     done
