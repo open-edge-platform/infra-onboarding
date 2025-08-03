d1a2722b68a9232320af1afb98f1dc88effe3295
Jayanthi, Shankar Srinivas <shankar.srinivas.jayanthi@intel.com>
2025-08-03 15:12:43 +0530

    Adding bootenty for harddisk boot

--- tinker-actions/src/efibootset/efibootset.sh
+++ tinker-actions/src/efibootset/efibootset.sh
@@ -3,45 +3,97 @@
 # SPDX-FileCopyrightText: (C) 2025 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-####################################################################################
-#delete the pile up HOOK OS partitions from bootMenu
-while IFS= read -r boot_part_number; do
-efibootmgr -b $boot_part_number -B
-done < <(efibootmgr | grep -i hookos | awk '{print $1}'| cut -c 5-8 )
+set -x
 
-#####################################################################################
-# ######## make PXE the last boot option possible.
-pxe_boot_number=$(efibootmgr | grep -i "Bootcurrent" | awk '{print $2}')
+efipart=""
 
-boot_order=$(efibootmgr | grep -i "Bootorder" | awk '{print $2}')
-
-# Convert boot_order to an array and remove , between the entries
-IFS=',' read -ra boot_order_array <<< "$boot_order"
-
-# Remove PXE boot entry from Array
-final_boot_array=()
-for element in "${boot_order_array[@]}"; do
-    if [[ "$element" != "$pxe_boot_number" ]]; then
-        final_boot_array+=("$element")
-    fi
+# Delete the pile up Ubuntu/Emt partitions from BIOS bootMenu
+echo "Deleting the boot entries from boot menu"
+for bootnumber in $(efibootmgr | grep -iE "Linux Boot Manager|Ubuntu" | awk '{print $1}' | sed 's/Boot//;s/\*//'); do
+    efibootmgr -b "$bootnumber" -B
 done
 
-# Add the PXE  boot entry to the end of the boot order array
-final_boot_array+=("$pxe_boot_number")
+# Get the pxe boot number
+pxe_boot_number=$(efibootmgr | grep -i "Bootcurrent" | awk '{print $2}')
 
-# Join the elements of boot_order_array into a comma-separated string
-final_boot_order=$(IFS=,; echo "${final_boot_array[*]}")
+# Delete the duplicate boot entries from bootmenu
+echo "Deleting the duplicate boot entries"
+boot_order=$(efibootmgr -D)
+echo "$boot_order"
 
-#remove trail and leading , if preset
-final_boot_order=$(echo "$final_boot_order" | sed -e  's/^,//;s/,$//' )
+# Get the rootfs && OS Type
+rootfs=$(blkid | grep -Ei 'TYPE="ext4"' | grep -Ei 'LABEL="cloudimg-rootfs"' | awk -F: '{print $1}')
+if [ -n "$rootfs" ]; then
+    os="Ubuntu"
+else
+    rootfs=$(blkid | grep -Ei 'TYPE="ext4"' | grep -Ei 'LABEL="rootfs"' | awk -F: '{print $1}')
+    os="Emt"
+fi
 
-echo "final_boot order--->" $final_boot_order
+efiboot=$(blkid | grep -Ei 'TYPE="vfat"' | grep -Ei 'LABEL="esp|uefi"' |  awk -F: '{print $1}')
 
-# Update the boot order using efibootmgr
-efibootmgr -o "$final_boot_order"
+if echo "$efiboot" | grep -q "nvme"; then
+    osdisk=$(echo "$rootfs" | grep -oE 'nvme[0-9]+n[0-9]+' | head -n 1)
+    efipart=$(echo "$rootfs" | awk 'NR==1 {print $1}' | awk -F'p' '{print $2}')
+elif echo "$efiboot" | grep -q "sd"; then
+    osdisk=$(echo "$rootfs" | grep -oE 'sd[a-z]+' | head -n 1)
+    efipart=$(echo "$rootfs" | awk 'NR==1 {print $1}' | sed 's/[^0-9]*//g')
+fi
 
-#Make UEFI boot as inactive 
-efibootmgr -b $pxe_boot_number -A
+if [ "$os" = "Emt" ]; then
+    
+    # Mount all required partitions to create bootctl install entry
+    mount "${rootfs}" /mnt
+    mount $efiboot /mnt/boot/efi
+    mount --bind /dev /mnt/dev
+    mount --bind /dev/pts /mnt/dev/pts
+    mount --bind /proc /mnt/proc
+    mount --bind /sys /mnt/sys
+    mount --bind /sys/firmware/efi/efivars /mnt/sys/firmware/efi/efivars
 
-echo "Made Disk as first boot and PXE boot order at end"
-# #####################################################################################
+    chroot /mnt /bin/bash <<EOT
+    set -e
+    bootctl install
+EOT
+
+    if [ "$?" -eq 0 ]; then
+        echo "Made Disk as first boot option"
+        #unmount the partitions
+        for mount in $(mount | grep '/mnt' | awk '{print $3}' | sort -nr); do
+            umount "$mount"
+        done
+    else
+        echo "Boot entry create failed,Please check!!"
+        #unmount the partitions
+        for mount in $(mount | grep '/mnt' | awk '{print $3}' | sort -nr); do
+            umount "$mount"
+        done
+        exit 1 
+    fi
+
+elif [ "$os" = "Ubuntu" ]; then
+
+    # Mount all required partitions to create boot entry
+    mount "${rootfs}" /mnt
+    if [ ! -d /mnt/boot/efi ]; then
+        mkdir -p /mnt/boot/efi
+    fi
+    mount "${efiboot}" /mnt/boot/efi
+
+    # Get the grubefi 
+    EFI=$(chroot /mnt sh -c 'find /boot/efi/EFI/ubuntu -type f -iname "shimx64.efi" | head -n1 | xargs -r basename') || exit 1
+    if [ -z "$EFI" ]; then
+        EFI="grubx64.efi"
+    fi
+    
+    umount /mnt/boot/efi
+    umount /mnt
+
+    new_boot_number=$(efibootmgr -c -d "/dev/${osdisk}" -p $efipart -L "Ubuntu" -l "\\EFI\\ubuntu\\$EFI") || { echo "Failed to create new boot entry"; exit 1; }
+    echo "Successfully created the boot entry"
+
+    #Make UEFI boot as inactive
+    efibootmgr -b $pxe_boot_number -A
+
+    echo "Made Disk as first boot option"
+fi
