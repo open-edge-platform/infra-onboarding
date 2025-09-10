9bc26a06a5e4ea03549f310eeb2f9297defcd1f4
punam biswal <punam.biswal@intel.com>
2025-09-10 15:10:25 +0530

    Get lvm size from instance resource and pass it to tinkeraction
    Signed-off-by: punam biswal <punam.biswal@intel.com>

--- onboarding-manager/internal/handlers/controller/reconcilers/instance.go
+++ onboarding-manager/internal/handlers/controller/reconcilers/instance.go
@@ -405,6 +405,7 @@ func convertInstanceToDeviceInfo(instance *computev1.InstanceResource,
 		HwSerialID:       host.GetSerialNumber(),
 		HwMacID:          host.GetPxeMac(),
 		HwIP:             host.GetBmcIp(),
+		LVMSize:          uint64(host.GetLvmSize()),
 		Hostname:         host.GetResourceId(), // we use resource ID as hostname to uniquely identify a host
 		SecurityFeature:  instance.GetSecurityFeature(),
 		OSImageURL:       osLocationURL,
@@ -415,6 +416,8 @@ func convertInstanceToDeviceInfo(instance *computev1.InstanceResource,
 		PlatformBundle:   desiredOs.GetPlatformBundle(),
 		IsStandaloneNode: isStandalone,
 	}
+
+	zlogInst.Info().Msgf("LVM size: %d", deviceInfo.LVMSize)
 
 	zlogInst.Debug().Msgf("DeviceInfo generated from OS resource (%s): %+v",
 		instance.GetDesiredOs().GetResourceId(), deviceInfo)
